68565a4c3d410d0787121ccea2ac5a510cbe0795
nodayuta_214136 <e214136ny@kurume-it.ac.jp>
2021-06-06 12:00:24 +0900

    音量取得と調節が全然できてません

---
+++ Assets/OptionScenes/vol.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class vol : MonoBehaviour
+{
+    AudioSource bgm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bgm = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bgm.volume = 0;
+    }
+}
