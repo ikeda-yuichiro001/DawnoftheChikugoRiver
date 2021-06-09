f8aab8ec960251bf5b5cc9d017677df9a66638d2
akano-shingo <e214101as@kurume-it.ac.jp>
2021-06-09 18:27:28 +0900

    Revert "更新"
    This reverts commit 647df9d3374a601c70b49d028213fefc0deb2c37.

--- Assets/DamageCnt/DamageCnt.cs
+++ Assets/DamageCnt/DamageCnt.cs
@@ -5,27 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class EnemyDamageCnt : MonoBehaviour
 {
-    Rigidbody rigidBody;
-    public Vector2 move;
-    float cnt;
-    public float LifeTime;
     public bool ishit;
     public bool IsPlayer;
     // Start is called before the first frame update
     void Start()
     {
-        rigidBody = GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        rigidBody.position += new Vector3(move.x, 0, move.y) * Time.deltaTime;
-        cnt += (Mathf.Abs(move.x) + Mathf.Abs(move.y)) * Time.deltaTime;
-        if (cnt > LifeTime)
-        {
-            Destroy(gameObject);
-        }
+        
     }
     public void OnTriggerStay(Collider other)//ダメージ処理
     {
