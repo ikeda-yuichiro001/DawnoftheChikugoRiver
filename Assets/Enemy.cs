fcb6994b4029d67e234c30162f2a4f3c5451281d
akano-shingo <e214101as@kurume-it.ac.jp>
2021-06-09 11:07:21 +0900

    ダメージ処理の敵が設定されていない問題解決しました。

---
+++ Assets/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    public int HP = 1000;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
