dfb4ae6a3c845049ce99b5c9d4ab53b46b075ef3
akano-shingo <e214101as@kurume-it.ac.jp>
2021-06-09 19:29:42 +0900

    ダメージ処理でHPが減ることを確認しました

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -4,17 +4,65 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    Rigidbody rigidBody;
+    public enum AttackPattern { Single, Double, Round }
+    public AttackPattern pattern;
+    public float cnt;
+    public float PlacementDistance;
+    [Range(0, 5)] public float Speed;
     public int HP = 1000;
     // Start is called before the first frame update
     void Start()
     {
-        rigidBody = GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (pattern)
+        {
+            case AttackPattern.Single:
+                {
+                    cnt += Time.deltaTime * Speed;
+                    if (cnt >= 1)
+                    {
+                        GameObject g = Instantiate(Resources.Load("Barrage_"), transform.position, Quaternion.identity) as GameObject;
+                        cnt = 0;
+                    }
+                }
+                break;
+
+
+
+
+            case AttackPattern.Double:
+                {
+                    cnt += Time.deltaTime * Speed;
+                    if (cnt >= 1)
+                    {
+                        Instantiate(Resources.Load("Barrage_"), transform.position + new Vector3(1, 0, 0) * PlacementDistance, Quaternion.identity);
+                        Instantiate(Resources.Load("Barrage_"), transform.position + new Vector3(-1, 0, 0) * PlacementDistance, Quaternion.identity);
+                        cnt = 0;
+                    }
+
+                }
+                break;
+
+            case AttackPattern.Round:
+                {
+                    cnt += Time.deltaTime * Speed;
+                    if (cnt >= 1)
+                    {
+                        for (int v = 0; v < 40; v++)
+                        {
+                            GameObject a = Instantiate(Resources.Load("Barrage_"), transform.position, Quaternion.identity) as GameObject;
+                            a.GetComponent<DamageCnt>().move = new Vector2(Mathf.Sin(v * 1f / 40 * Mathf.PI * 2), Mathf.Cos(v * 1f / 40 * Mathf.PI * 2));
+                        }
+                        cnt = 0;
+                    }
+                }
+                break;
+        }
+
     }
 }
