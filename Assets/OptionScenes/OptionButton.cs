a7b4dc03bc76bc5baea57b62d7c5c9432a320a92
nodayuta_214136 <e214136ny@kurume-it.ac.jp>
2021-06-10 12:50:56 +0900

    もう少しで終わりそうです

--- Assets/OptionScenes/OptionButton.cs
+++ Assets/OptionScenes/OptionButton.cs
@@ -15,34 +15,28 @@ public class OptionButton : MonoBehaviour
     Text BGM;
     Text SE;
     Text Default;
-    Text Quit;
-    public static int point = 0;
-    public Text Textbgmvol;
-    protected float bgmvol;
-    public Text Textsevol;
-    protected float sevol;
-    AudioSource bgm;
-    AudioSource se;
+    public  int point = 0;
+    /*AudioSource bgm;
+    AudioSource se;*/
 
     void Start()
     {
-        bgmvol = 0;
-        sevol = 0;
         Decision = GameObject.Find("Canvas/Panel/Decision").GetComponent<Image>();
         BGM = GameObject.Find("Canvas/Panel/Decision/BGM").GetComponent<Text>();
         SE = GameObject.Find("Canvas/Panel/Decision/SE").GetComponent<Text>();
         Default = GameObject.Find("Canvas/Panel/Decision/Default").GetComponent<Text>();
-        Quit = GameObject.Find("Canvas/Panel/Decision/Quit").GetComponent<Text>();
         Number = GameObject.Find("Canvas/Panel/Decision/Number").GetComponent<Text>();
         Number2 = GameObject.Find("Canvas/Panel/Decision/Number2").GetComponent<Text>();
-        bgm = GetComponent<AudioSource>();
-        se = GetComponent<AudioSource>();
+        
+        /*bgm = GameObject.Find("BGMvolume").GetComponent<AudioSource>();
+        se = GameObject.Find("SEvolume").GetComponent<AudioSource>();*/
     }
 
     //サウンド設定Buttonの色変化と決定キーを押したときの処理
     void Update()
     {
-
+        Number.text = ((int)(Option.BGM * 100)).ToString("000");
+        Number2.text = ((int)(Option.SE * 100)).ToString("000");
 
         //BGM等の画面の処理
         if (Input.GetKeyDown(KeyCode.UpArrow))
@@ -54,14 +48,14 @@ public class OptionButton : MonoBehaviour
             point++;
         }
 
-        if (point == 5)
+        if (point == 4)
         {
             point = 0;
         }
 
         if (point == -1)
         {
-            point = 4;
+            point = 3;
         }
 
 
@@ -94,17 +88,7 @@ public class OptionButton : MonoBehaviour
             Default.color = new Color32(70, 70, 70, 100);
         }
 
-
-        if (point == 3)
-        {
-            Quit.color = Color.red;
-        }
-        else
-        {
-            Quit.color = new Color32(70, 70, 70, 100);
-        }
-
-        if(point==4)
+        if(point == 3)
         {
             Decision.color = Color.red;
         }
@@ -115,61 +99,68 @@ public class OptionButton : MonoBehaviour
 
 
         //決定Buttonを押したらタイトル画面へ
-        if (Input.GetKeyDown(KeyCode.Z))
+        if (point == 3)
         {
-            if (point == 4)
+            if (Input.GetKeyDown(KeyCode.Z))
             {
                 SceneManager.LoadScene("Title");
-            }
 
+            }
         }
+        
 
         //BGMのText数字増減
-        Textbgmvol.text = string.Format("vol {0:000} ",bgmvol);
         if (point == 0)
         {
             if (Input.GetKeyDown(KeyCode.RightArrow))
             {
-                Option.BGM ++;
+                Option.BGM += 0.1f;
+                Debug.Log(Option.BGM);
             }
             if (Input.GetKeyDown(KeyCode.LeftArrow))
             {
-                Option.BGM --;
+                Option.BGM -= 0.1f;
+                Debug.Log(Option.BGM);
+
             }
         }
 
-        if (Option.BGM < 0) Option.BGM = 0;
-        if (Option.BGM > 100) Option.BGM = 100;
+        //if (Option.BGM < 0.0f) Option.BGM = 0.0f;
+        //if (Option.BGM > 100.0f) Option.BGM = 100.0f;
 
 
         //SEのText数字増減
-        Textsevol.text = string.Format("vol {0:000} ", sevol);
         if (point == 1)
         {
             if (Input.GetKeyDown(KeyCode.RightArrow))
             {
-                Option.SE++;
+                Option.SE += 0.1f;
+                Debug.Log(Option.SE);
             }
             if (Input.GetKeyDown(KeyCode.LeftArrow))
             {
-                Option.SE--;
+                Option.SE -= 0.1f;
+                Debug.Log(Option.SE);
             }
         }
 
-        if (Option.SE < 0) Option.SE = 0;
-        if (Option.SE > 100) Option.SE = 100;
+        //if (Option.SE < 0.0f) Option.SE = 0.0f;
+        //if (Option.SE > 100.0f) Option.SE = 100.0f;
         
 
         //Defalutの処理
         if(point == 2 && Input.GetKeyDown(KeyCode.Z))
         {
-            Option.BGM = 50;
-            Option.SE = 50;
+            Option.BGM = 0.1f;
+            Option.SE = 0.1f;
+            Debug.Log(Option.BGM);
+            Debug.Log(Option.SE);
         }
 
+        
 
         //BGMのvolume調整
-        if (point == 0)
+        /*if (point == 0)
         {
             if (Input.GetKeyDown(KeyCode.RightArrow))
             {
@@ -203,7 +194,7 @@ public class OptionButton : MonoBehaviour
         if (point == 2 && Input.GetKeyDown(KeyCode.Z))
         {
             se.volume = 0.50f;
-        }
+        }*/
     }
 }
 
@@ -221,7 +212,7 @@ public class Option
         }
         set
         {
-            if (value > 100) bgm = 100;
+            if (value > 1) bgm = 1;
             else if (value < 0) bgm = 0;
             else bgm = value;
         }
@@ -237,7 +228,7 @@ public class Option
         }
         set
         {
-            if (value > 100) se = 100;
+            if (value > 1) se = 1;
             else if (value < 0) se = 0;
             else se = value;
         }
