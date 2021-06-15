f413091093145b92a9ef124686de552b2ab53a12
akano-shingo <e214101as@kurume-it.ac.jp>
2021-06-15 11:04:55 +0900

    Logo画面復旧完了title画面に移動できることを確認

---
+++ Assets/LogoScens/Fade.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Fade : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Image>().enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Z))
+        {
+            SceneManager.LoadScene("title");
+        }
+    }
+}
