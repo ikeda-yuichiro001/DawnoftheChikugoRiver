bd138e3756f1f1e56bd94f0aa727665976920606
MimuraYuuki <e214147my@kurume-it.ac.jp>
2021-06-16 17:28:50 +0900

    ゲームシーンとムービーシーンのシーン移動を

---
+++ Assets/SceneChange/Movie Scene.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class MovieScene : MonoBehaviour
+{
+    VideoPlayer RawImage;
+    int StageNumber;
+    float time = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RawImage = GameObject.Find("RawImage").GetComponent<VideoPlayer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        RawImage.url = "file///mov+StageNumber+.mp4";
+        if (Time.deltaTime >= /ムービーの長さ/ +1)
+        {
+            if (SceneLoader.IsFade) return;
+            SceneLoader.Load("stage+StageNumber");
+        }
+    }
+}
+
+public class next
+{
+    int StageNumber ++;
+        if(StageNumber == 5)
+        {
+            if (SceneLoader.IsFade) return;
+            SceneLoader.Load("GameClear");
+        }
+        else
+        {
+            RawImage.url = "file///mov+StageNumber+.mp4";
+        }  
