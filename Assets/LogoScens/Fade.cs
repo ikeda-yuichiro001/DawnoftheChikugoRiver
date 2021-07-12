92924085d26369928320ffc921af31556f4f36b9
akano-shingo <e214101as@kurume-it.ac.jp>
2021-07-12 17:23:34 +0900

    ***************

--- Assets/LogoScens/Fade.cs
+++ Assets/LogoScens/Fade.cs
@@ -2,18 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Fade : MonoBehaviour
 {
+    public float speed = 0.005f;
+    float alfa;
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Image>().enabled = true;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        GetComponent<Image>().color = new Color(255, 255, 255, alfa);
+        alfa += speed;
+        if (alfa >= 1)
+        {
+            speed = -0.005f;
+            
+        }
+        if (alfa < 0)
+        {
+            SceneManager.LoadScene("title");
+        }
+        Debug.Log(alfa);
     }
 }
