9ec757bf57b1342dff45dce4283fa3bdbebaa19c
akano-shingo <e214101as@kurume-it.ac.jp>
2021-07-12 16:31:07 +0900

    タスク管理の黄色い矢印の部分のデバックをしたよ

--- Assets/LogoScens/Scene.cs
+++ Assets/LogoScens/Scene.cs
@@ -1,19 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class Fade : MonoBehaviour
+public class Scene : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Image>().enabled = true;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKey(KeyCode.Z))
+        {
+            SceneManager.LoadScene("title");
+        }
     }
 }
