4129123792cde4547e90e43ace6a53bab4c23716
nodayuta_214136 <e214136ny@kurume-it.ac.jp>
2021-08-05 15:20:19 +0900

    LogoからTitleへシーン遷移してもBGMが流れるように修正中

---
+++ Assets/LogoScens/BGM.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BGM : MonoBehaviour
+{
+    static bool Destroy = true;
+    
+    void Start()
+    {
+        if (Destroy)
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    
+    void Update()
+    {
+       
+    }
+}
---
+++ Assets/Title/Object.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object : MonoBehaviour
+{
+    static bool Destroy = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
