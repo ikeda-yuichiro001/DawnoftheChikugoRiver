6ac09d0ff83a0871b9052dd80f0e8f691aa3cf1f
akano-shingo <e214101as@kurume-it.ac.jp>
2021-08-19 12:56:19 +0900

    クラス名を変えたよ

--- Assets/LogoScens/MoveScene.cs
+++ Assets/LogoScens/MoveScene.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Scene : MonoBehaviour
+public class MoveScene : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
