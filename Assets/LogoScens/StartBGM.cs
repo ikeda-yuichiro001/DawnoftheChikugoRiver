4f1e372d9c15d41c5592d967fd34f6c0f2a0374d
nodayuta_214136 <e214136ny@kurume-it.ac.jp>
2021-08-20 16:06:02 +0900

    LogoシーンにBGMを付けました

---
+++ Assets/LogoScens/StartBGM.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartBGM : MonoBehaviour
+{
+    public bool destroy = true;
+    
+    void Start()
+    {
+        if (destroy)
+        {
+            //シーン遷移してもオブジェクトが消えない
+            DontDestroyOnLoad(gameObject);
+        }
+        SceneManager.sceneLoaded += SceneLoad;
+    }
+
+    void SceneLoad(Scene nextScene, LoadSceneMode mode)
+    {
+        if(SceneManager.GetActiveScene().name == "Difficulty")
+        {
+            SceneManager.sceneLoaded -= SceneLoad;
+            Destroy(gameObject);
+        }
+    }
+}
