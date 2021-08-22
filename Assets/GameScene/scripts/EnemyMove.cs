851e93bb6d000021d97c038636ca5cfc440c63c6
KIT19F1S073\Owner <e194140nh@gmail.com>
2021-08-22 19:51:11 +0900

    とりあえず弾幕パターンまとめた

--- Assets/GameScene/scripts/EnemyMove.cs
+++ Assets/GameScene/scripts/EnemyMove.cs
@@ -96,22 +96,122 @@ public class EnemyMove : MonoBehaviour
     }
 
     void enemymove1() {
-    //public 
     bool mirror = false;
     int MirrorDirection;
     int w = 0, a = 0;
-    //public 
-    int hp = 500;
-    float cnt = 0;
-    //public 
-    int t = 8;
-    //public 
+    int hp = 300;
+    float cnt;
+    int t = 40;
     float Speed = 3;
-    //public 
     float PlacementDistance = 3;
-    GameObject Player = GameObject.Find("player"); ;
+    GameObject Player;
     float d = 100;
-    Rigidbody rb = GetComponent<Rigidbody>();
+    // Start is called before the first frame update
+    void _Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        Player = GameObject.Find("player");
+    }
+
+    // Update is called once per frame
+    void _Update()
+    {
+        MirrorDirection = 1;
+        if (mirror) MirrorDirection = -1;
+
+        cnt += Time.deltaTime * Speed * DifficultyScene.difspd * DifficultyScene.difspd * 0.2f;                        //new
+        if (cnt >= 1)                                  //new
+        {
+            GameObject aa = Instantiate(Resources.Load("enemy_bul"), transform.position, Quaternion.identity) as GameObject; //new
+            aa.GetComponent<enemyShotPattern>().arrow = new Vector2(0, -DifficultyScene.difspd * 0.3f);
+            cnt = 0;
+        }
+
+        GetComponent<Rigidbody>().position += new Vector3(-0.3f * MirrorDirection, 0, 0);
+
+        if (transform.position.x < -27)
+            transform.position = new Vector3(27, transform.position.y, transform.position.z);
+        if (transform.position.x > 27)
+            transform.position = new Vector3(-27, transform.position.y, transform.position.z);
+
+        if (hp <= 0)
+        {
+
+            while (w < 50)
+            {
+                //Debug.Log(a);
+                //Debug.Log(w);
+
+                if (a % 2 == 0)
+                {
+                    Instantiate(Resources.Load("Item"), transform.position + new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10)), Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(Resources.Load("PointItem"), transform.position + new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10)), Quaternion.identity);
+                }
+                a = Random.Range(0, 99);
+                w = Random.Range(0, 99);
+            }
+
+            imageTest.kari += 100;
+            imageTest.scorejudge = 1;
+            Destroy(gameObject);
+        }
+
+        if (Player != null)
+        {
+            d = Vector3.Distance(transform.position, Player.transform.position);
+        }
+
+        if (d < 2.5f)
+        {
+            shot.PowData = Player.gameObject.GetComponent<shot>().Power;
+            Destroy(Player);
+            Debug.Log("ピチューン！");
+        }
+
+    }
+}
+
+    void enemymove2()
+    {
+
+    }
+    void enemymove3()
+    {
+
+    }
+    void enemymove4()
+    {
+
+    }
+    void enemymove5()
+    {
+
+    }
+    void enemymove6()
+    {
+
+    }
+    void enemymove7()
+    {
+        //public 
+        bool mirror = false;
+        int MirrorDirection;
+        int w = 0, a = 0;
+        //public 
+        int hp = 500;
+        float cnt = 0;
+        //public 
+        int t = 8;
+        //public 
+        float Speed = 3;
+        //public 
+        float PlacementDistance = 3;
+        GameObject Player = GameObject.Find("player"); ;
+        float d = 100;
+        Rigidbody rb = GetComponent<Rigidbody>();
         MirrorDirection = 1;
         if (mirror) MirrorDirection = -1;
 
@@ -151,7 +251,7 @@ public class EnemyMove : MonoBehaviour
             imageTest.scorejudge = 1;
             Destroy(gameObject);
         }
-
+        //ここを画面外から見えなくなったらにする
         if (transform.position.z < -player_ctrl.zlimit)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, player_ctrl.zlimit);
@@ -173,31 +273,6 @@ public class EnemyMove : MonoBehaviour
             Destroy(Player);
             Debug.Log("ピチューン！");
         }
-    
-
-}
-    void enemymove2()
-    {
-
-    }
-    void enemymove3()
-    {
-
-    }
-    void enemymove4()
-    {
-
-    }
-    void enemymove5()
-    {
-
-    }
-    void enemymove6()
-    {
-
-    }
-    void enemymove7()
-    {
 
     }
     void enemymove8()
