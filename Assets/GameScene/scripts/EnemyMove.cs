1bb1414e6191b14a8f98ecb01ef1753e95eced9d
KIT19F1S073\Owner <e194140nh@gmail.com>
2021-08-22 21:05:27 +0900

    とりあえずいまある弾幕パターンをまとめた

--- Assets/GameScene/scripts/EnemyMove.cs
+++ Assets/GameScene/scripts/EnemyMove.cs
@@ -184,8 +184,88 @@ public class EnemyMove : MonoBehaviour
     }
     void enemymove4()
     {
+    int w = 0, a = 0;
+    int maxhp = 3000;
+    int hp = maxhp;
+    float cnt;
+    int t = 40;
+    float Speed;
+    float PlacementDistance;
+    GameObject Player;
+    GameObject layer;
+    float d = 100;
+    
 
+    // Start is called before the first frame update
+    void _Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        layer = GameObject.Find("player");
+        hp = maxhp;
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player_ctrl.pc != null)
+            Player = player_ctrl.pc.gameObject;
+        //Debug.Log(1);
+
+        //gameObject.transform.position = 
+        GetComponent<Rigidbody>().position += new Vector3(Mathf.Sin(Time.time * Random.Range(2, 4))/*+0.1f*/, 0, /*Mathf.Cos(Time.time*4)*-1*/0);
+        cnt += Time.deltaTime * Speed * DifficultyScene.difspd * DifficultyScene.difspd * 0.1f;
+
+        if (cnt >= 1)                                  //new
+        {
+            Vector3 jikinerai = Player.transform.position - transform.position;
+            jikinerai.Normalize();
+
+            GameObject a4 = Instantiate(Resources.Load("enemy_bul_big"), transform.position, Quaternion.identity) as GameObject;
+            a4.GetComponent<enemyShotPattern>().arrow = new Vector2(jikinerai.x, jikinerai.z) * DifficultyScene.difspd * 0.4f;
+            //Debug.Log(a.GetComponent<enemyShotPattern>().arrow = new Vector2(jikinerai.x, jikinerai.z));
+            cnt = 0;
+        }
+
+        if (transform.position.z > 35 || transform.position.z < -35 || transform.position.x > 35 || transform.position.x < -35)
+            transform.position = new Vector3(0, transform.position.y, transform.position.z);
+        //Debug.Log(hp);
+
+        if (hp <= 0)
+        {
+
+            while (w < 50)
+            {
+                //Debug.Log(a);
+                //Debug.Log(w);
+
+                if (a % 2 == 0)
+                {
+                    Instantiate(Resources.Load("Item"), transform.position + new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10)), Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(Resources.Load("PointItem"), transform.position + new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10)), Quaternion.identity);
+                }
+                a = Random.Range(0, 99);
+                w = Random.Range(0, 99);
+            }
+
+            Destroy(gameObject);
+        }
+
+        if (layer != null)
+        {
+            d = Vector3.Distance(transform.position, layer.transform.position);
+        }
+
+        if (d < 2.5f)
+        {
+            shot.PowData = layer.gameObject.GetComponent<shot>().Power;
+            Destroy(layer);
+            Debug.Log("ピチューン！");
+        }
+    }
+}
     void enemymove5()
     {
 
@@ -220,8 +300,8 @@ public class EnemyMove : MonoBehaviour
         {
             for (int v = 0; v < t * DifficultyScene.difspd; v++)
             {
-                GameObject aa = Instantiate(Resources.Load("enemy_bul"), transform.position, Quaternion.identity) as GameObject; //new
-                aa.GetComponent<enemyShotPattern>().arrow = new Vector2(Mathf.Sin(v * 1f / t * Mathf.PI * 2), Mathf.Cos(v * 1f / t * Mathf.PI * 2)) * DifficultyScene.difspd * Speed * PlacementDistance / 15;
+                GameObject a7 = Instantiate(Resources.Load("enemy_bul"), transform.position, Quaternion.identity) as GameObject; //new
+                a7.GetComponent<enemyShotPattern>().arrow = new Vector2(Mathf.Sin(v * 1f / t * Mathf.PI * 2), Mathf.Cos(v * 1f / t * Mathf.PI * 2)) * DifficultyScene.difspd * Speed * PlacementDistance / 15;
             }
             cnt = 0;
         }
