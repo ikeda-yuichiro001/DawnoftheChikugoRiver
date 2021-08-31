8daeb27d31bde5ea10302ab22f16666fa5850219
KIT19F1S073\Owner <e194140nh@gmail.com>
2021-08-31 20:45:27 +0900

    各ステージの敵生成プログラムの雛型作った

---
+++ Assets/GameScene/scripts/xxx/EnemyGenelator1.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGenelator1 : EnemyGenelatorBehavior
+{
+    public GenerateData[] enemyList = new GenerateData[]
+    {
+        new GenerateData(){ name = "hinamoroko_model1r", pointX = -18, pointZ = 30, timer = 1},
+        new GenerateData(){ name = "hinamoroko_model1r", pointX = -9, pointZ = 27,timer = 3},
+        new GenerateData(){ name = "hinamoroko_model1r", pointX = 0, pointZ = 24,timer = 5},
+        new GenerateData(){ name = "hinamoroko_model1r", pointX = 9, pointZ = 21,timer = 7},
+        new GenerateData(){ name = "hinamoroko_model1r", pointX = 18, pointZ = 18,timer = 9},
+
+        new GenerateData(){ name = "kawamutu1", pointX = 12, pointZ = 30, timer = 14},
+        new GenerateData(){ name = "kawamutu1", pointX = 12, pointZ = 30, timer = 16},
+        new GenerateData(){ name = "kawamutu1", pointX = 12, pointZ = 30 ,timer = 18},
+        new GenerateData(){ name = "kawamutu1", pointX = 12, pointZ = 30, timer = 20},
+        new GenerateData(){ name = "kawamutu1", pointX = 12, pointZ = 30, timer = 22},
+
+        new GenerateData(){ name = "kawamutu1", pointX = -12, pointZ = 30, timer = 14},
+        new GenerateData(){ name = "kawamutu1", pointX = -12, pointZ = 30, timer = 16},
+        new GenerateData(){ name = "kawamutu1", pointX = -12, pointZ = 30, timer = 18},
+        new GenerateData(){ name = "kawamutu1", pointX = -12, pointZ = 30, timer = 20},
+        new GenerateData(){ name = "kawamutu1", pointX = -12, pointZ = 30, timer = 22},
+    };
+    public bool IsCreateBoss = false;
+    public GameObject boss;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public override void UPDATE()
+    {
+        enemyList = CreateByTimer(enemyList);
+        if (IsAllDead(enemyList) && !IsCreateBoss)
+        {
+            IsCreateBoss = true;
+            boss = Generate("kurumeusu2", 0,0);
+        }
+
+        if(IsCreateBoss && boss == null)
+        {
+            //GameClear
+        }
+
+    }
+}
