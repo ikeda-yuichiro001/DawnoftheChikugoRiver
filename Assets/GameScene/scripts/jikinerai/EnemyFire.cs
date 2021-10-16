fdfde1d7fbe816cfc4051644c596a3dd7fd821bc
KIT19F1S073\Owner <e194140nh@gmail.com>
2021-10-17 03:29:26 +0900

    ----

---
+++ Assets/GameScene/scripts/jikinerai/EnemyFire.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFire : MonoBehaviour
+{
+    public GameObject enemyMissilePrefab;
+    GameObject enemyMissile;
+    public float speed;
+    private int timeCount = 0;
+
+    void Update()
+    {
+        timeCount += 1;
+
+        if (timeCount % 30 == 0)
+        {
+            // 敵のミサイルを生成する
+            enemyMissile = Instantiate(enemyMissilePrefab, transform.position, Quaternion.identity);
+
+            Rigidbody enemyMissileRb = enemyMissile.GetComponent<Rigidbody>();
+
+            // ミサイルを飛ばす方向を決める。「forward」は「z軸」方向をさす（ポイント）
+            enemyMissileRb.AddForce(transform.forward * speed);
+
+
+        }
+    }
+}
--- Assets/GameScene/scripts/jikinerai/Lookat.cs
+++ Assets/GameScene/scripts/jikinerai/Lookat.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LookAt : MonoBehaviour
+public class Lookat : MonoBehaviour
 {
     private GameObject target;
 
